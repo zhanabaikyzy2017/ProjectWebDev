230dfd7eddd69c62538ac63828f5daf873d90766
Your Name <sidakinaddd@gmail.com>
2019-05-09 17:59:12 +0500

    routing login

--- front/easyLib/src/app/app-routing.module.ts
+++ front/easyLib/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { LoginComponent} from './login/login.component';
+
+const routes: Routes = [
+  { path: '', component:MainComponent},
+  {path:'login',component:LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front/easyLib/src/app/app.module.ts
+++ front/easyLib/src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
+import { LoginComponent } from './login/login.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
