575c95457713ea803cd206bca1fa8c2370655638
Your Name <sidakinaddd@gmail.com>
2019-05-09 12:39:17 +0500

    dana's front with interfaces

---
+++ front/easyLib/src/app/main/models/models.ts
@@ -0,0 +1,44 @@
+export interface IAuthor{
+    name:string;
+    surname:string;
+    date_of_birth:any;
+}
+
+export interface IPublisher{
+    name:string;
+}
+
+export interface IUser{
+    userID:number;
+    username:string;
+    password:string;
+    email:string;
+}
+export interface IUserProfile{
+    user: IUser;
+    mobile:string;
+    websilte:string;
+    join_date:any;
+}
+
+export interface ICategory{
+    name:string;
+}
+
+export interface IBook{
+    title:string;
+    category:any;
+    description:string;
+    year:number;
+    author: IAuthor
+    publisher:IPublisher
+    page_amount:number;
+    added_by:IUserProfile
+}
+
+export interface IQuotation{
+    user:IUser;
+    book:IBook;
+    quotation:string;
+    creation_date:any;
+}
